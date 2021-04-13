169e82d9ad1fbfbeafc4ecdac7cf90135409e15a
Filip Ilic <filip@filipilic.com>
2021-04-13 11:17:13 +0000

    initial commit

---
+++ marketing/marketing/doctype/marketing_position/marketing_position.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Filip Ilić and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class MarketingPosition(NestedSet):
+	pass
+
---
+++ marketing/marketing/doctype/platform/platform.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Filip Ilić and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Platform', {
+	// refresh: function(frm) {
+
+	// }
+});
